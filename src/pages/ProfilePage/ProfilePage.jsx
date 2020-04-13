07a45e2cbcb451ef4e245df3e367c1ca350e1177
rjohnson0707 <rwilljohnson@icloud.com>
2020-04-13 14:59:14 -0600

    New NavBar, links throughout work, profile page renders expected user prop

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./ProfilePage.css";
 
 const ProfilePage = (props) => {
   return (
-    <div>
-      <h1>This is {props.user.name}'s Profile Page</h1>
+    <div className="Profile">
+      <div className="Profile-h1">
+        <h1>{props.user.name}'s Profile</h1>
+      </div>
+      <div className="Profile-info">
+        <h4>Name: {props.user.name}</h4>
+        <h4>Email: {props.user.email}</h4>
+        <h5>Edit Info</h5>
+      </div>
     </div>
   );
 };
