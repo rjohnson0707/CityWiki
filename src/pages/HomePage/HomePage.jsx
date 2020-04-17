cd50947ff43775b6bf9819b04f4ed9c4a6b2840a
rjohnson0707 <rwilljohnson@icloud.com>
2020-04-17 12:11:40 -0600

    Fixed Margin-Top issue with all pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,18 @@ import "./HomePage.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 const HomePage = (props) => {
-  return (
+  let main = props.user ? (
     <div className="HomePage">
       <SearchBar />
     </div>
+  ) : (
+    <div className="HomePage">
+      <h1 style={{ marginTop: 0 }}>Welcome To CityWiki!</h1>
+      <h4>Please Log In or Sign Up in order to begin your City Searches</h4>
+    </div>
   );
+
+  return <div>{main}</div>;
 };
 
 export default HomePage;
