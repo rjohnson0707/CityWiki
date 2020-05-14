9514ea95a5ac97ad3f9126b3048a2ab1c36190a1
rjohnson0707 <rwilljohnson@icloud.com>
2020-05-14 09:36:14 -0600

    New Style for city pages, fixed bugs for API fetch from NYtimes

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,7 +8,7 @@ class Map extends React.Component {
     if (this.props.lat && this.props.lng) {
       const location = { lat: this.props.lat, lng: this.props.lng };
       const map = new window.google.maps.Map(this.mapDiv.current, {
-        zoom: this.props.zoom || 12,
+        zoom: this.props.zoom || 10,
         center: location,
         disableDefaultUI: true,
         styles: mapStyle,
